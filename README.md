23c7a03121b7401db060bd6a3a20f9b86aaaedd9
Daniel Gómez Jurado <danielgomjur@gmail.com>
2014-11-22 03:06:16 +0100

    Working. No excuses

---
+++ README.md
@@ -0,0 +1,5 @@
+Little time saver to check which class I have to go :)
+
+TODO:
+  - Nested sections
+  - Show on top today's class
--- src/Pebble-ETSIIT.c
+++ src/Pebble-ETSIIT.c
@@ -1,43 +1,151 @@
 #include <pebble.h>
 
 static Window *window;
-static TextLayer *text_layer;
+static MenuLayer *menu_layer;
 
-static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Select");
+
+static void menu_draw_row_callback(GContext* ctx, const Layer *cell_layer, MenuIndex *cell_index, void *data) {
+  switch (cell_index->section) {
+    case 0:
+      // Use the row to specify which item we'll draw
+      switch (cell_index->row) {
+        case 0:
+          // This is a basic menu item with a title and subtitle
+          menu_cell_title_draw(ctx, cell_layer, "FR - 3.7");
+          break;
+
+        case 1:
+          // This is a basic menu icon with a cycling icon
+          menu_cell_title_draw(ctx, cell_layer, "IG - 1.1");
+          break;
+      }
+      break;
+
+    case 1:
+      switch (cell_index->row) {
+        case 0:
+          // There is title draw for something more simple than a basic menu item
+          menu_cell_title_draw(ctx, cell_layer, "ISE - 2.3");
+          break;
+        case 1:
+          // There is title draw for something more simple than a basic menu item
+          menu_cell_title_draw(ctx, cell_layer, "FR - 1.1");
+          break;
+      }
+      break;
+
+    case 2:
+      switch (cell_index->row) {
+        case 0:
+          // There is title draw for something more simple than a basic menu item
+          menu_cell_title_draw(ctx, cell_layer, "DDSI - 3.3");
+          break;
+        case 1:
+          // There is title draw for something more simple than a basic menu item
+          menu_cell_title_draw(ctx, cell_layer, "ISE - 1.1");
+          break;
+      }
+      break;
+
+    case 3:
+      switch (cell_index->row) {
+        case 0:
+          // There is title draw for something more simple than a basic menu item
+          menu_cell_title_draw(ctx, cell_layer, "DDSI - 3.5");
+          break;
+        case 1:
+          // There is title draw for something more simple than a basic menu item
+          menu_cell_title_draw(ctx, cell_layer, "DDSI - 1.1");
+          break;
+        case 2:
+          // There is title draw for something more simple than a basic menu item
+          menu_cell_title_draw(ctx, cell_layer, "MC - 2.2");
+          break;
+      }
+      break;
+
+    case 4:
+      switch (cell_index->row) {
+        case 0:
+          // There is title draw for something more simple than a basic menu item
+          menu_cell_title_draw(ctx, cell_layer, "IG - 2.3");
+          break;
+        case 1:
+          // There is title draw for something more simple than a basic menu item
+          menu_cell_title_draw(ctx, cell_layer, "MC - 1.1");
+          break;
+      }
+      break;
+  }
 }
 
-static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Up");
+static int16_t menu_get_header_height_callback(MenuLayer *menu_layer, uint16_t section_index, void *data) {
+  // This is a define provided in pebble.h that you may use for the default height
+  return MENU_CELL_BASIC_HEADER_HEIGHT;
 }
 
-static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Down");
+static uint16_t menu_get_num_sections_callback(MenuLayer *menu_layer, void *data) {
+  return 5;
 }
 
-static void click_config_provider(void *context) {
-  window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
-  window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
-  window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
+static uint16_t menu_get_num_rows_callback(MenuLayer *menu_layer, uint16_t section_index, void *data) {
+  if (section_index == 3) return 3;
+
+  return 2;
+}
+
+static void menu_draw_header_callback(GContext* ctx, const Layer *cell_layer, uint16_t section_index, void *data) {
+  // Determine which section we're working with
+  switch (section_index) {
+    case 0:
+      // Draw title text in the section header
+      menu_cell_basic_header_draw(ctx, cell_layer, "Monday");
+      break;
+
+    case 1:
+      menu_cell_basic_header_draw(ctx, cell_layer, "Tuesday");
+      break;
+
+    case 2:
+      menu_cell_basic_header_draw(ctx, cell_layer, "Wednesday");
+      break;
+
+    case 3:
+      menu_cell_basic_header_draw(ctx, cell_layer, "Thursday");
+      break;
+
+    case 4:
+      menu_cell_basic_header_draw(ctx, cell_layer, "Friday");
+      break;
+  }
 }
 
 static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
-  text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } });
-  text_layer_set_text(text_layer, "Press a button");
-  text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
-  layer_add_child(window_layer, text_layer_get_layer(text_layer));
+  menu_layer = menu_layer_create(bounds);
+
+  menu_layer_set_callbacks(menu_layer, NULL, (MenuLayerCallbacks) {
+    .get_num_sections = menu_get_num_sections_callback,
+    .get_num_rows = menu_get_num_rows_callback,
+    .draw_row = menu_draw_row_callback,
+    .get_header_height = menu_get_header_height_callback,
+    .draw_header = menu_draw_header_callback,
+  });
+
+  menu_layer_set_click_config_onto_window(menu_layer, window);
+
+  layer_add_child(window_layer, menu_layer_get_layer(menu_layer));
 }
 
 static void window_unload(Window *window) {
-  text_layer_destroy(text_layer);
+  menu_layer_destroy(menu_layer);
 }
 
 static void init(void) {
   window = window_create();
-  window_set_click_config_provider(window, click_config_provider);
+
   window_set_window_handlers(window, (WindowHandlers) {
     .load = window_load,
     .unload = window_unload,
